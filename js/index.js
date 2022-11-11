717f8555f6d1aeff689e9c74b7ac25a84874d8a3
gremlin654 <sidelnikov99@gmail.com>
2022-11-11 13:50:32 +0300

    add response style

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ const descriptionBtn2 = document.querySelector('#description-btn2');
 const descriptionText2 = document.querySelector('#description-text2');
 const descriptionBtn3 = document.querySelector('#description-btn3');
 const descriptionText3 = document.querySelector('#description-text3');
+const header = document.querySelector('.header');
 
 const addClassDescriptions = (btn, text) => {
   text.classList.toggle('descriptional-more');
@@ -29,3 +30,65 @@ descriptionBtn2.addEventListener('click', () =>
 descriptionBtn3.addEventListener('click', () =>
   addClassDescriptions(descriptionBtn3, descriptionText3)
 );
+
+document.addEventListener('scroll', () => {
+  if (scrollY > 0) {
+    header.classList.add('active');
+  } else header.classList.remove('active');
+});
+
+const iconMenu = document.querySelector('.burger__menu');
+const menuHeader = document.querySelector('.menu__list');
+const menuOverlay = document.querySelector('.burger__menu-overlay');
+// const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+// const logoHeader = document.querySelector('.header__logo-container');
+// const logoBurger = document.querySelector('.burger-logo');
+
+if (iconMenu) {
+  iconMenu.addEventListener('click', () => {
+    document.body.classList.toggle('lock');
+    iconMenu.classList.toggle('active-burger');
+    menuHeader.classList.toggle('active');
+    menuOverlay.classList.toggle('burger__menu-overlay-active');
+    header.classList.toggle('active-menu-burger');
+    // logoHeader.classList.toggle('unactive-logo');
+    // logoBurger.classList.toggle('active-logo');
+  });
+}
+
+const buttonCallback = document.querySelectorAll('[data-call]');
+const modalCallback = document.querySelector('.modal__form-callback');
+const closeButtons = document.querySelectorAll('[data-close]');
+const buttonRequest = document.querySelectorAll('[data-request]');
+const modalRequest = document.querySelector('.modal__form-request');
+
+buttonCallback.forEach((button) =>
+  button.addEventListener('click', () => {
+    modalCallback.classList.toggle('active');
+  })
+);
+
+closeButtons.forEach((button) =>
+  button.addEventListener('click', (e) => {
+    modalCallback.classList.remove('active');
+    modalRequest.classList.remove('active');
+  })
+);
+
+buttonRequest.forEach((button) =>
+  button.addEventListener('click', () => {
+    modalRequest.classList.toggle('active');
+  })
+);
+
+modalCallback.addEventListener('click', (e) => {
+  if (e.target.classList.contains('modal__form-callback')) {
+    modalCallback.classList.toggle('active');
+  }
+});
+
+modalRequest.addEventListener('click', (e) => {
+  if (e.target.classList.contains('modal__form-request')) {
+    modalRequest.classList.toggle('active');
+  }
+});
